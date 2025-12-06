3ed28297ce6d3dac5449c53d06478146c62e486a
Vinay Malyala <srivinay1432@gmail.com>
2025-12-06 19:38:08 +0530

    database and admin panel

--- src/main/admin.py
+++ src/main/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from .models import Topic, Room, Message
+
 # Register your models here.
+class TopicAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Topic, TopicAdmin)
+
+
+class RoomAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Room, RoomAdmin)
+
+
+class MessageAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Message, MessageAdmin)
--- src/main/views.py
+++ src/main/views.py
@@ -1,22 +1,16 @@
 from django.shortcuts import render
 
+from .models import Room
+
 # Create your views here.
 
-rooms = [
-    {"id": 1, "name": "Python Room"},
-    {"id": 2, "name": "Django Room"},
-    {"id": 3, "name": "Java Room"},
-]
-
 def home_view(request):
+    rooms = Room.objects.all()
     context = {"rooms": rooms}
     return render(request, 'main/home.html', context)
 
 
 def room_view(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'main/room.html', context)
