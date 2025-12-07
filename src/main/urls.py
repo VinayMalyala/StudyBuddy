b08c7a0e3e1108ad6d716e3e0de959932c06229e
Vinay Malyala <srivinay1432@gmail.com>
2025-12-07 10:52:51 +0530

    user login

--- src/main/urls.py
+++ src/main/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import home_view, room_view, create_room_view, update_room_view, delete_room_view
+from .views import home_view, room_view, create_room_view, update_room_view, delete_room_view, login_view
 
 urlpatterns = [
+    path("login/", login_view, name="login"),
+
     path('', home_view, name='home'),
     path('room/<str:pk>/', room_view, name='room'),
+
     path('create-room/', create_room_view, name="create-room"),
     path('update-room/<str:pk>', update_room_view, name="update-room"),
     path('delete-room/<str:pk>', delete_room_view, name="delete-room"),
--- src/main/views.py
+++ src/main/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render, redirect
 from django.db.models import Q
+from django.contrib.auth.models import User
 
 from .models import Room, Topic
 from .forms import RoomForm
 
 # Create your views here.
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            pass
+    context = {}
+    return render(request, 'main/login_register.html', context)
 
 def home_view(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
